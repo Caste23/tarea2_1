d76f461c23e5e3094e030f510d6471e6c631726b
wilme castellanos <castellanoswilmer20@gmail.com>
2022-07-07 18:30:03 -0600

    Agregar archivos de proyecto.

---
+++ tarea2_1/tarea2_1/controller/countri.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using tarea2_1.models;
+namespace tarea2_1.controller
+{
+    internal class countri
+    {
+        public static string endpoint = "https://restcountries.com/v3.1/region/";
+
+        //hacemos un contructor vacio
+        public countri()
+        {
+
+        }
+
+        public async Task<List<countriesmodel>> GetCountries(string region)
+        {
+            try
+            {
+                List<countriesmodel> countries = new List<countriesmodel>();
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var resp = await client.GetAsync(endpoint + region);
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        var content = resp.Content.ReadAsStringAsync().Result;
+
+                        JArray results = JArray.Parse(content);
+
+                        countriesmodel temp = null;
+
+                        foreach (var item in results)
+                        {
+                            temp = new countriesmodel();
+
+                            var curTemp = new List<countriesmodel.Currencies>();
+                            foreach (var current in item["currencies"].Values())
+                            {
+                                try
+                                {
+                                    curTemp.Add(new countriesmodel.Currencies()
+                                    {
+                                        name = current["name"].ToString(),
+                                        symbol = current["symbol"].ToString()
+                                    });
+                                }
+                                catch
+                                {
+                                    curTemp.Add(new countriesmodel.Currencies()
+                                    {
+                                        name = current["name"].ToString(),
+                                        symbol = "--"
+                                    });
+                                }
+                            }
+
+                            var languTemp = new List<string>();
+                            foreach (var current in item["languages"].Values())
+                                languTemp.Add(current.ToString());
+
+                            var timeTemp = new List<string>();
+                            foreach (var current in item["timezones"].Values())
+                                timeTemp.Add(current.ToString());
+
+                            var contTemp = new List<string>();
+                            foreach (var current in item["continents"].Values())
+                            {
+                                contTemp.Add(current.ToString());
+                            }
+
+                            temp.NameCountry = new countriesmodel.Name()
+                            {
+                                common = item["name"]["common"].ToString(),
+                                official = item["name"]["official"].ToString()
+
+                            };
+
+                            try
+                            {
+                                temp.independent = item["independent"].ToString(); // EUROPA
+                            }
+                            catch
+                            {
+                                temp.independent = "--";
+                            }
+
+                            temp.status = item["status"].ToString();
+                            temp.currencies = curTemp;
+
+                            try
+                            {
+                                temp.capital = item["capital"][0].ToString(); // ASIA
+                            }
+                            catch
+                            {
+                                temp.capital = "--";
+                            }
+
+                            temp.region = item["region"].ToString();
+                            temp.subregion = item["subregion"].ToString();
+                            temp.languages = languTemp;
+                            temp.latlng = new List<double>() { (double)item["latlng"][0], (double)item["latlng"][1] };
+                            temp.flag = item["flag"].ToString();
+                            temp.maps = new countriesmodel.Maps()
+                            {
+                                googleMaps = item["maps"]["googleMaps"].ToString(),
+                                openStreetMaps = item["maps"]["openStreetMaps"].ToString()
+                            };
+
+                            temp.population = (int)item["population"];
+                            temp.timezones = timeTemp;
+                            temp.continents = contTemp;
+
+                            temp.flags = new countriesmodel.Flags()
+                            {
+                                png = item["flags"]["png"].ToString(),
+                                svg = item["flags"]["svg"].ToString()
+                            };
+
+                            temp.startOfWeek = item["startOfWeek"].ToString();
+
+                            try
+                            {
+                                temp.idd = new countriesmodel.Idd()
+                                {
+                                    root = item["idd"]["root"].ToString(),
+                                    suffixes = item["idd"]["suffixes"][0].ToString()
+                                };
+                            }
+                            catch
+                            {
+                                temp.idd = new countriesmodel.Idd()
+                                {
+                                    root = "-",
+                                    suffixes = "---"
+                                };
+
+                            }
+                            countries.Add(temp);
+                        }
+                    }
+
+                }
+                return countries.OrderBy(x => x.NameCountry.common).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return null;
+            }
+
+        }
+    }
+
+}
---
+++ tarea2_1/tarea2_1/views/otro.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+namespace tarea2_1.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class otro : ContentPage
+
+    {
+        countriesmodel model1;
+        public otro(countriesmodel model)
+        {
+            model1 = model;
+            InitializeComponent();
+            loadConfiguration();
+
+        }
+        private async void loadConfiguration()
+        {
+
+            var pin = new Pin()
+            {
+                Type = PinType.SavedPin,
+                Position = new Position(countrySelected.latlng[0], countrySelected.latlng[1]),
+                Label = "Country",
+                Address = countrySelected.NameCountry.official
+            };
+
+            mapa.Pins.Add(pin);
+            mapa.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(countrySelected.latlng[0], countrySelected.latlng[1]), Distance.FromKilometers(41)));
+        }
+    }
+}
